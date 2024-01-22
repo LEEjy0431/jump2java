48a3494bb21ac6949c1edfd91cc96c17f36a6836
leejy <jys0431@gmail.com>
2024-01-22 10:25:07 +0900

    page1

---
+++ src/main/java/com/mysite/jump2java/Jump2javaApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.jump2java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jump2javaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jump2javaApplication.class, args);
+    }
+
+}
